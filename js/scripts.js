f9fdee831289ecbec7213409ff4ac83430edfd51
Advent1013 <advent1013@live.com>
2017-06-10 16:39:45 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(window).resize(function() {
+	var dev = "images/devblg.png";
+	var adv = "images/advblg.png";
+	var lan = "images/lanblg.png";
+     if($(this).width() < 450){
+	    	dev = "images/devblgtiny.png";
+		adv = "images/advblgtiny.png";
+		lan = "images/lanblgtiny.png";
+	}
+	$("#devimg").attr('src',dev);
+	$("#advimg").attr('src',adv);
+	$("#lanimg").attr('src',lan);
+});
