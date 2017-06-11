9701a5721880f993952ff590e30ba739eef5c425
Advent1013 <advent1013@live.com>
2017-06-11 04:48:32 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 console.log("Could this be an easter egg?");
-var landingbg = 1 + Math.floor(Math.random() * 2);
+var maxImgs = 2;
+var landingbg = 1 + Math.floor(Math.random() * maxImgs);
   
 $(window).resize(function() {
 	var dev = "images/devblg.png";
 	var adv = "images/advblg.png";
 	var lan = "images/lanblg.png";
-    if($(this).width() < 450){
-	    dev = "images/devblgtiny.png";
+	if($(this).width() < 450){
+		dev = "images/devblgtiny.png";
 		adv = "images/advblgtiny.png";
 		lan = "images/lanblgtiny.png";
 	}
@@ -15,3 +16,13 @@ $(window).resize(function() {
 	$("#lanimg").attr('src',lan);
 });
 $('#landing').css("background-image", "url(images/landingbg/"+landingbg+".gif)");
+
+function selectBG(){
+	var tempbg = 1 + Math.floor(Math.random() * maxImgs);
+	while(tempbg == landingbg){
+		tempbg = 1 + Math.floor(Math.random() * maxImgs);
+	}
+	landingbg = tempbg;
+	$('#landing').css("background-image", "url(images/landingbg/"+landingbg+".gif)");
+}
+$("landingclick").click(function(){selectBG()});
